17c2cc46b15cd7c169605dee578386dcf31e7ccb
Kyle Dean Reinford <kyledeanreinford@gmail.com>
2023-06-13 12:04:39 -0500

    Create app_personalization.sh

---
+++ app_personalization.sh
@@ -0,0 +1,44 @@
+echo "Running Personalized Script"
+
+# List of apps to install
+declare -a apps=( \
+    1password-cli \
+    defaultbrowser
+)
+
+for package in "${apps[@]}"
+do
+  echo "Installing ${package}"
+  brew install ${package}
+done
+
+# List of trusted cask apps to install
+declare -a caskApps=( \
+    spotify \
+    signal \
+    dropbox \
+    notion \
+    nordvpn \
+    adobe-creative-cloud \
+    warp
+)
+
+for package in "${caskApps[@]}"
+do
+  echo "Installing ${package}"
+  brew install --cask --no-quarantine ${package}
+done
+
+
+# List of apps to remove
+declare -a toRemove=( \
+    sublime-text \
+    
+)
+
+for package in "${toRemove[@]}"
+do
+  echo "Removing ${package}"
+  brew uninstall ${package}
+  brew autoremove
+done

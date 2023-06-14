401ec4c03f4c1f64926dfe378dea3784b280962b
Kyle Dean Reinford <kyledeanreinford@gmail.com>
2023-06-14 09:26:58 -0500

    Update preferences.sh

--- preferences.sh
+++ preferences.sh
@@ -1,4 +1,4 @@
-echo "Loading personal preferences from dotfile"
+echo "Loading personal preferences"
 
 # set key repeat speed
 defaults write NSGlobalDomain KeyRepeat -int 4
@@ -14,8 +14,44 @@ defaults write com.apple.screencapture location -string "$HOME/Desktop"
 defaults write NSGlobalDomain AppleShowAllExtensions -bool true
 
 # Add folders to sidebar
-mysides add Desktop file:///Users/$accountname/Desktop
-mysides add Documents file:///Users/$accountname/Documents
-mysides add Downloads file:///Users/$accountname/Downloads
+mysides add Desktop
+mysides add Documents
+mysides add Downloads
+
+# List of apps to open
+declare -a apps=( \
+    1password \
+    Rectangle \
+    NordVPN \
+    Notion \
+    Slack \
+    Pastebot \ 
+    Dropbox
+)
+
+for package in "${apps[@]}"
+do
+  echo "Installing ${package}"
+  open ${package}.app
+done
+
+# add permanent dock items
+defaults write com.apple.dock persistent-apps -array
+
+defaults write com.apple.dock persistent-apps -array-add '<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>Applications/Google Chrome.app</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>'
+defaults write com.apple.dock persistent-apps -array-add '<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>/Applications/IntelliJ IDEA.app</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>'
+
+
+declare -a dockItems=( \
+  Slack.app \
+  Signal.app \
+  Warp.app \
+  Notion.app \
+  Pop.app
+)
+for item in "${dockItems[@]}"
+do
+  defaults write com.apple.dock persistent-apps -array-add '<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>'Applications/$item'</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>'
+done; killall Dock
 
 mkdir ~/Dev
